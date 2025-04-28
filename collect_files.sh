046cdf48839ebdc1758b70fdd2339e4349dd27c2
Maria Shagako <mashagako@mail.ru>
2025-04-28 21:50:24 +0300

    3 change

--- collect_files.sh
+++ collect_files.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
 input_dir="$1"
 output_dir="$2"
-all_files=$(find "$input_dir" -type f)
 
-for file in $all_files; do
-filename=$(basename "$file")
-
-copy () {
-	local dir="$1"
-	for file in "$dir"/*; do
-		if [ -d "$file"]; then
-			copy "$file"
-	elif [ -f "$file" ]; then
-		local filename=$(basename "$file")
-		local file2="$output_dir/$filename"
-		local k=1
-
-		while [-e "$file2" ]; do
-			file2="$output_dir/${filename%.*}_$k.${filename##*.}"
-			((k++))
-		done
-		cp "$file" "$file2"
-	fi
-done
