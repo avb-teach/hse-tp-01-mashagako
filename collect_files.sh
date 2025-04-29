43dd4ff546f25343b14dfc4de3d2a1f66b1ab7af
Maria Shagako <mashagako@mail.ru>
2025-04-29 03:39:25 +0300

    6change

--- collect_files.sh
+++ collect_files.sh
@@ -2,7 +2,7 @@
 input_dir="$1"
 output_dir="$2"
 
-find "$input_dir" -type f -exec cp {} "$output_dir" \;
+#find "$input_dir" -type f -exec cp {} "$output_dir" \;
 
 find "$input_dir" -type f -print0 | while IFS= read -r -d $'\0' file; do
 	filename=$(basename "$file")
